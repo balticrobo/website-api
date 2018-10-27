120a4f230febe068448a34b6640e19042c0ee382
Paweł Cierzniakowski <pawel@cierzniakowski.pl>
2018-10-27 23:14:04 +0200

    Add monolog

--- config/bundles.php
+++ config/bundles.php
@@ -5,6 +5,7 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true],
 ];
