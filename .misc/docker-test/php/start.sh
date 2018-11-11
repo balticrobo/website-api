c370a8242ee7ce0b276f848cf90afbf5ff91aaf4
Paweł Cierzniakowski <pawel@cierzniakowski.pl>
2018-11-11 18:45:24 +0100

    Fix tests runner script

--- .misc/docker-test/php/start.sh
+++ .misc/docker-test/php/start.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
-composer install
+function run {
+  echo "############ $1 ############" | tr '_' ' '
+  shift
+  eval $@
+  quit_when_fail
+}
 
-bin/console lint:yaml config
-vendor/bin/phpcs -p
-vendor/bin/var-dump-check --symfony --laravel --doctrine src
+function quit_when_fail {
+  if [[ $? -ne 0 ]]; then
+    echo "> Task failed!"
+    exit 1
+  fi
+}
+
+run Install_vendors_from_Composer composer install
+run Lint_YAML_configs bin/console lint:yaml config
+run PHP_CodeSniffer vendor/bin/phpcs -p
+run VarDump_Check vendor/bin/var-dump-check --symfony src
